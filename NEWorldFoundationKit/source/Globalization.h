1c0f3783dd1ba1d08d30e4b4d3900c469b417581
予顺 刘 <yshliu0321@icloud.com>
2016-04-08 17:21:31 +0800

    Multiple Changes

---
+++ NEWorldFoundationKit/source/Globalization.h
@@ -0,0 +1,23 @@
+#ifndef GLOBALIZATION_H
+#define GLOBALIZATION_H
+#include "stdinclude.h"
+
+namespace Globalization
+{
+struct Line
+{
+    string str;
+    int id;
+};
+
+extern int count;
+extern map<int, Line> Lines;
+extern map<string, int> keys;
+extern string Cur_Lang;
+
+bool LoadLang(string lang);
+bool Load();
+string GetStrbyid(int id);
+string GetStrbyKey(string key);
+}
+#endif
