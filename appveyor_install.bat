7b238276fc785462391608068e7a14c9eae64498
Null <abc612008@users.noreply.github.com>
2016-08-22 20:55:16 -0700

    Update appveyor_install.bat

--- appveyor_install.bat
+++ appveyor_install.bat
@@ -1,4 +1,4 @@
-# Install Boost libraries
+:: Install Boost libraries
 mkdir boost
 cd boost\
 appveyor-retry nuget install boost_system-vc140 -Version 1.61.0 > nul
@@ -6,10 +6,10 @@ appveyor-retry nuget install boost_filesystem-vc140 -Version 1.61.0 > nul
 appveyor-retry nuget install boost_date_time-vc140 -Version 1.61.0 > nul
 appveyor-retry nuget install boost_regex-vc140 -Version 1.61.0 > nul
 cd ..
-# Install other dependencies
+:: Install other dependencies
 appveyor DownloadFile https://raw.githubusercontent.com/Infinideastudio/NEWorld-binaries/master/dependencies.exe
 C:\NEWorld\dependencies.exe > nul
-# Link
+:: Link
 mklink /D dependencies\include\boost C:\NEWorld\boost\boost.1.61.0.0\lib\native\include\boost
 mkdir dependencies\lib\boostlib
 copy C:\NEWorld\boost\boost_filesystem-vc140.1.61.0.0\lib\native\address-model-32\lib\libboost_filesystem-vc140-mt-gd-1_61.lib dependencies\lib\boostlib
